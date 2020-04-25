008f39f6e1c14599c0036a8ab8d286c0767609ea
Can YILDIRIM <mcanyildirim@gmail.com>
2020-04-25 13:27:03 +0100

    frontend work is initiated

---
+++ Controller/Index/Index.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace Fetchtex\FAQs\Controller;
+
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    /**
+     * Index resultPageFactory
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Index constructor.
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct( Context $context, PageFactory $resultPageFactory )
+    {
+        $this->resultPageFactory = $resultPageFactory;
+        return parent::__construct($context);
+    }
+
+    /**
+     * Execute action based on request and return result
+     *
+     * Note: Request will be added as operation argument in future
+     *
+     * @return \Magento\Framework\Controller\ResultInterface|ResponseInterface
+     * @throws \Magento\Framework\Exception\NotFoundException
+     */
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
